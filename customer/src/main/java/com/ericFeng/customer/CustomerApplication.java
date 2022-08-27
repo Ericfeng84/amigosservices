5dc21dbf313755d5df6d2145978196df21847323
fengkang <fengkang84@outlook.com>
2022-08-27 20:58:04 +0800

    K8S

--- customer/src/main/java/com/ericFeng/customer/CustomerApplication.java
+++ customer/src/main/java/com/ericFeng/customer/CustomerApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 
 @SpringBootApplication(
         scanBasePackages = {
@@ -14,6 +16,10 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 )
 @EnableEurekaClient
 @EnableFeignClients(basePackages = "com.EricFeng.clients")
+@PropertySources({
+        @PropertySource("classpath:clients-${spring.profiles.active}.properties")
+})
+
 public class CustomerApplication {
     public static void main(String[] args) {
         SpringApplication.run(CustomerApplication.class, args);
--- fraud/src/main/java/com/EricFeng/fraud/FraudApplication.java
+++ fraud/src/main/java/com/EricFeng/fraud/FraudApplication.java
@@ -3,9 +3,20 @@ package com.EricFeng.fraud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 
-@SpringBootApplication
+@SpringBootApplication(
+        scanBasePackages = {
+                "com.EricFeng.fraud",
+                "com.EricFeng.clients"
+        }
+)
 @EnableEurekaClient
+@PropertySources({
+        @PropertySource("classpath:clients-${spring.profiles.active}.properties")
+})
+
 public class FraudApplication {
     public static void main(String[] args) {
         SpringApplication.run(FraudApplication.class, args);
